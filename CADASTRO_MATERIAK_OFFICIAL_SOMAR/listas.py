2ae67d943d4c4a383cabda29c3e3b51dff0ffe60
Valdemir Vieira Alves <feveralves@gmail.com>
2022-10-25 12:53:57 -0300

    primeiro envio do arquivo cadastro material

---
+++ CADASTRO_MATERIAK_OFFICIAL_SOMAR/listas.py
@@ -0,0 +1,14 @@
+
+
+lista_material = ['Areia Lavada','Anel de Concreto',
+                    '','Bica Fina','Bica Grossa','Brita 0','Bloco de 10','Bloco de 15','Bloco de 20','Boca de Lobo',
+                    '','Cimento',
+                    '','Manilha 300','Manilha 400','Manilha 600','Malh Pop','Meio Fio',
+                    '','Vergalhão 5/16','Vergalhão 1/4' ,'Vergalãho 4.2','Vergalhão 3/8',
+                    '','Prego 17x27','','Tábua de 30']
+
+quantidade = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,
+              36,37,38,39,40,41,42,43,44,45,46,47,48,49,50]
+
+tipo = ['Unidade','M³','Kg']
+
